56e84e8a715eadb6119b9f11dc067d8539f39e3a
Huddy2022 <mlvnhud@aol.com>
2022-12-21 20:04:19 +0000

    add two functions for the user to create their own board size and number of ships

--- run.py
+++ run.py
@@ -8,8 +8,23 @@ def new_game():
 
         user_name = input("Please enter your name: \n")
         print("-" * 35)
-        
+
         return user_name
 
 
+def board_size(user_name):
+    board_size = input(f"{user_name} please select a board size between 5-10: \n")
+    print("-" * 35)
+
+    return board_size
+
+
+def number_of_ships(user_name):
+    number_of_ships = input(f"{user_name} please select how many ships in the game between 4-8: \n")
+    print("-" * 35)
+
+    return number_of_ships()
+
 print(new_game())
+print(board_size(user_name))
+print(number_of_ships(user_name))
