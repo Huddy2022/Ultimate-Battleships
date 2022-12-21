3061401f78e4bad294faccd3e62d9ba25b9524b7
Huddy2022 <mlvnhud@aol.com>
2022-12-21 19:57:26 +0000

    inital commit

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def new_game():
+    """
+    Start a new game and get users name, board size and number of ships
+    they want to use in the game
+    """
+    while True:
+        print("Welcome to Ultimate Battleships")
+
+        user_name = input("Please enter your name: \n")
+        print("-" * 35)
+        
+        return user_name
+
+
+print(new_game())
