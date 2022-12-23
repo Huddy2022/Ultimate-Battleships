f8ea178f7ce9b2ced043ecd1abc69496aad95c58
Huddy2022 <mlvnhud@aol.com>
2022-12-23 12:14:46 +0000

    add a number of ships function for the user and new add_ships function for the game

--- run.py
+++ run.py
@@ -10,16 +10,23 @@ def new_game():
     while True:
         user_name = input("Please enter your name: \n")
         print("-" * 35)
-        board_size = input(f"{user_name} please select a board size between 5 - 10: \n")
+        board_size = input(f"{user_name} please select a board size between 5-10: \n")
         if validate_board_size(board_size) is True:
             print("-" * 35)
+            return board_size
         else:
             return False
-        number_of_ships = input(f"{user_name} please select how many ships in the game between 4-8: \n")
+
+
+def number_of_ships():
+    """
+    Let user select how many ships they would like to play with between 4-8
+    """
+    while True:
+        number_of_ships = input("Please select how many ships in the game between 4-8: \n")
         if validate_number_of_ships(number_of_ships) is True:
             print("-" * 35)
-            print(user_name, board_size, number_of_ships)
-            return board_size
+            return number_of_ships
         else:
             return False
 
@@ -53,18 +60,34 @@ def validate_number_of_ships(value):
 
 
 def create_board(board_size):
+    """
+    Create the board using the size the user selected.
+    Create a list and place O for each space
+    """
     return [["O" for count in range(int(board_size))] for count in range(int(board_size))]
 
 
 def print_board(board):
+    """
+    Print the board using the create_board function and 
+    print the list elements in single lines with space
+    """
     for b in board:
         print(*b)
 
 
+def add_ships(ships):
+
+
 def main():
+    """
+    Main function to run the game
+    """
     data = new_game()
     create_board(data)
     board = create_board(data)
+    ships = number_of_ships()
+    add_ships(ships)
     print_board(board)
 
 
